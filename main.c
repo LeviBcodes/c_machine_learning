57bed416467b17eba36b8c3e0fbc592b600a342e
LeviBcodes <burland.levi@gmail.com>
2023-09-03 07:14:15 -0600

    working on memory allocation

--- main.c
+++ main.c
@@ -97,37 +97,25 @@ Layer create_random_layer(int num_neurons, int num_weights) {
     return layer;
 }
 
-// Function to create a data structure with memory error handling
+// function to create a data
 Data create_data(int num_inputs, int num_outputs) {
     Data data;
     data.inputs = malloc(num_inputs * sizeof(double));
     data.outputs = malloc(num_outputs * sizeof(double));
-
-    if (data.inputs == NULL || data.outputs == NULL) {
-        printf("Error: malloc failed\n");
-        exit(1);
-    }
-
     return data;
 }
 
-// Function to create a training set with memory error handling
+// function to create a training set
 TrainingSet create_training_set(int num_data, int num_inputs, int num_outputs) {
     TrainingSet training_set;
     training_set.num_data = num_data;
     training_set.data = malloc(num_data * sizeof(Data));
-
-    if (training_set.data == NULL) {
-        printf("Error: malloc failed\n");
-        exit(1);
-    }
-
     for (int i = 0; i < num_data; i++) {
         training_set.data[i] = create_data(num_inputs, num_outputs);
     }
-
     return training_set;
 }
+
 
 // function to generate a random double between 0 and 1
 double random_double() {
@@ -229,4 +217,274 @@ double *network_output(Network *network, double *inputs) {
         }
     }
     return outputs;
+}
+
+int main()
+{
+
+    srand(time(NULL));
+
+    // Create a network
+    Network network;
+    network.num_layers = 3;
+    network.layers = malloc(network.num_layers * sizeof(Layer));
+    network.layers[0] = create_layer_with_random_weights(2, 2);
+    network.layers[1] = create_layer_with_random_weights(2, 2);
+    network.layers[2] = create_layer_with_random_weights(1, 2);
+
+
+    // Create a training set
+    TrainingSet training_set = create_training_set(4, 2, 1);
+    set_training_inputs(&training_set, (double[]){0.0, 0.0}, 0);
+    set_training_outputs(&training_set, (double[]){0.0}, 0);
+    set_training_inputs(&training_set, (double[]){0.0, 1.0}, 1);
+    set_training_outputs(&training_set, (double[]){1.0}, 1);    
+    set_training_inputs(&training_set, (double[]){1.0, 0.0}, 2);
+    set_training_outputs(&training_set, (double[]){1.0}, 2);
+    set_training_inputs(&training_set, (double[]){1.0, 1.0}, 3);
+    set_training_outputs(&training_set, (double[]){0.0}, 3);
+
+
+    // Train the network
+    double *outputs;
+    double *errors;
+    double *layer_inputs;
+    double *layer_outputs;
+    double *layer_errors;
+    double *layer_weights;
+    double *layer_weights_deltas;
+    double *layer_bias_deltas;
+    double *layer_weights_deltas_sum;
+    double *layer_bias_deltas_sum;
+    double *layer_weights_deltas_avg;
+    double *layer_bias_deltas_avg;
+    double *layer_weights_deltas_sum_prev;
+    double *layer_bias_deltas_sum_prev;
+    double *layer_weights_deltas_avg_prev;
+    double *layer_bias_deltas_avg_prev;
+
+    double learning_rate = 0.1;
+    double momentum = 0.9;
+    int num_epochs = 10000;
+    int num_data = training_set.num_data;
+    int num_layers = network.num_layers;
+    int num_neurons;
+    int num_weights;
+    int num_inputs;
+    int num_outputs;
+    int num_weights_deltas;
+    int num_bias_deltas;
+    int num_weights_deltas_sum;
+    int num_bias_deltas_sum;
+    int num_weights_deltas_avg;
+    int num_bias_deltas_avg;
+    int num_weights_deltas_sum_prev;
+    int num_bias_deltas_sum_prev;
+    int num_weights_deltas_avg_prev;
+    int num_bias_deltas_avg_prev;
+    
+    // Initialize the weights deltas
+    for (int i = 0; i < num_layers; i++) {
+        num_neurons = network.layers[i].num_neurons;
+        num_weights = sizeof(network.layers[i].neurons[0].weights) / sizeof(double);
+        num_weights_deltas = num_neurons * num_weights;
+        num_bias_deltas = num_neurons;
+        num_weights_deltas_sum = num_weights_deltas;
+        num_bias_deltas_sum = num_bias_deltas;
+        num_weights_deltas_avg = num_weights_deltas;
+        num_bias_deltas_avg = num_bias_deltas;
+        num_weights_deltas_sum_prev = num_weights_deltas;
+        num_bias_deltas_sum_prev = num_bias_deltas;
+        num_weights_deltas_avg_prev = num_weights_deltas;
+        num_bias_deltas_avg_prev = num_bias_deltas;
+        layer_weights_deltas = malloc(num_weights_deltas * sizeof(double));
+        layer_bias_deltas = malloc(num_bias_deltas * sizeof(double));
+        layer_weights_deltas_sum = malloc(num_weights_deltas_sum * sizeof(double));
+        layer_bias_deltas_sum = malloc(num_bias_deltas_sum * sizeof(double));
+        layer_weights_deltas_avg = malloc(num_weights_deltas_avg * sizeof(double));
+        layer_bias_deltas_avg = malloc(num_bias_deltas_avg * sizeof(double));
+        layer_weights_deltas_sum_prev = malloc(num_weights_deltas_sum_prev * sizeof(double));
+        layer_bias_deltas_sum_prev = malloc(num_bias_deltas_sum_prev * sizeof(double));
+        layer_weights_deltas_avg_prev = malloc(num_weights_deltas_avg_prev * sizeof(double));
+        layer_bias_deltas_avg_prev = malloc(num_bias_deltas_avg_prev * sizeof(double));
+        for (int j = 0; j < num_weights_deltas; j++) {
+            layer_weights_deltas[j] = 0.0;
+        }
+        for (int j = 0; j < num_bias_deltas; j++) {
+            layer_bias_deltas[j] = 0.0;
+        }
+        for (int j = 0; j < num_weights_deltas_sum; j++) {
+            layer_weights_deltas_sum[j] = 0.0;
+        }
+        for (int j = 0; j < num_bias_deltas_sum; j++) {
+            layer_bias_deltas_sum[j] = 0.0;
+        }
+        for (int j = 0; j < num_weights_deltas_avg; j++) {
+            layer_weights_deltas_avg[j] = 0.0;
+        }
+
+        for (int j = 0; j < num_bias_deltas_avg; j++) {
+            layer_bias_deltas_avg[j] = 0.0;
+        }
+        for (int j = 0; j < num_weights_deltas_sum_prev; j++) {
+            layer_weights_deltas_sum_prev[j] = 0.0;
+        }
+        for (int j = 0; j < num_bias_deltas_sum_prev; j++) {
+            layer_bias_deltas_sum_prev[j] = 0.0;
+        }
+        for (int j = 0; j < num_weights_deltas_avg_prev; j++) {
+            layer_weights_deltas_avg_prev[j] = 0.0;
+        }
+        for (int j = 0; j < num_bias_deltas_avg_prev; j++) {
+            layer_bias_deltas_avg_prev[j] = 0.0;
+        }
+        network.layers[i].neurons[0].weights = layer_weights_deltas;
+        network.layers[i].neurons[0].weights = layer_bias_deltas;
+        network.layers[i].neurons[0].weights = layer_weights_deltas_sum;
+        network.layers[i].neurons[0].weights = layer_bias_deltas_sum;
+        network.layers[i].neurons[0].weights = layer_weights_deltas_avg;
+        network.layers[i].neurons[0].weights = layer_bias_deltas_avg;
+        network.layers[i].neurons[0].weights = layer_weights_deltas_sum_prev;
+        network.layers[i].neurons[0].weights = layer_bias_deltas_sum_prev;
+        network.layers[i].neurons[0].weights = layer_weights_deltas_avg_prev;
+        network.layers[i].neurons[0].weights = layer_bias_deltas_avg_prev;
+    }
+
+    // Train the network
+    for (int i = 0; i < num_epochs; i++) {
+        for (int j = 0; j < num_data; j++) {
+            num_inputs = sizeof(training_set.data[j].inputs) / sizeof(double);
+            num_outputs = sizeof(training_set.data[j].outputs) / sizeof(double);
+            outputs = network_output(&network, training_set.data[j].inputs);
+            errors = malloc(num_outputs * sizeof(double));
+            for (int k = 0; k < num_outputs; k++) {
+                errors[k] = training_set.data[j].outputs[k] - outputs[k];
+            }
+            for (int k = num_layers - 1; k >= 0; k--) {
+                num_neurons = network.layers[k].num_neurons;
+                num_weights = sizeof(network.layers[k].neurons[0].weights) / sizeof(double);
+                num_weights_deltas = num_neurons * num_weights;
+                num_bias_deltas = num_neurons;
+                num_weights_deltas_sum = num_weights_deltas;
+                num_bias_deltas_sum = num_bias_deltas;
+                num_weights_deltas_avg = num_weights_deltas;
+                num_bias_deltas_avg = num_bias_deltas;
+                num_weights_deltas_sum_prev = num_weights_deltas;
+                num_bias_deltas_sum_prev = num_bias_deltas;
+                num_weights_deltas_avg_prev = num_weights_deltas;
+                num_bias_deltas_avg_prev = num_bias_deltas;
+                layer_inputs = malloc(num_inputs * sizeof(double));
+                layer_outputs = malloc(num_neurons * sizeof(double));
+                layer_errors = malloc(num_neurons * sizeof(double));
+                layer_weights = malloc(num_weights * sizeof(double));
+                layer_weights_deltas = malloc(num_weights_deltas * sizeof(double));
+                layer_bias_deltas = malloc(num_bias_deltas * sizeof(double));
+                layer_weights_deltas_sum = malloc(num_weights_deltas_sum * sizeof(double));
+                layer_bias_deltas_sum = malloc(num_bias_deltas_sum * sizeof(double));
+                layer_weights_deltas_avg = malloc(num_weights_deltas_avg * sizeof(double));
+                layer_bias_deltas_avg = malloc(num_bias_deltas_avg * sizeof(double));
+                layer_weights_deltas_sum_prev = malloc(num_weights_deltas_sum_prev * sizeof(double));
+                layer_bias_deltas_sum_prev = malloc(num_bias_deltas_sum_prev * sizeof(double));
+                layer_weights_deltas_avg_prev = malloc(num_weights_deltas_avg_prev * sizeof(double));
+                layer_bias_deltas_avg_prev = malloc(num_bias_deltas_avg_prev * sizeof(double));
+                for (int l = 0; l < num_inputs; l++) {
+                    layer_inputs[l] = training_set.data[j].inputs[l];
+                }
+                for (int l = 0; l < num_neurons; l++) {
+                    layer_outputs[l] = network.layers[k].neurons[l].output;
+                }
+                for (int l = 0; l < num_neurons; l++) {
+                    layer_errors[l] = errors[l];
+                }
+                for (int l = 0; l < num_weights; l++) {
+                    layer_weights[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_weights_deltas; l++) {
+                    layer_weights_deltas[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_bias_deltas; l++) {
+                    layer_bias_deltas[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_weights_deltas_sum; l++) {
+                    layer_weights_deltas_sum[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_bias_deltas_sum; l++) {
+                    layer_bias_deltas_sum[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_weights_deltas_avg; l++) {
+                    layer_weights_deltas_avg[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_bias_deltas_avg; l++) {
+                    layer_bias_deltas_avg[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_weights_deltas_sum_prev; l++) {
+                    layer_weights_deltas_sum_prev[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_bias_deltas_sum_prev; l++) {
+                    layer_bias_deltas_sum_prev[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_weights_deltas_avg_prev; l++) {
+                    layer_weights_deltas_avg_prev[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_bias_deltas_avg_prev; l++) {
+                    layer_bias_deltas_avg_prev[l] = network.layers[k].neurons[0].weights[l];
+                }
+                for (int l = 0; l < num_neurons; l++) {
+                    for (int m = 0; m < num_weights; m++) {
+                        layer_weights_deltas[l * num_weights + m] = layer_inputs[m] * layer_errors[l] * sigmoid_derivative(layer_outputs[l]);
+                    }
+                }
+                for (int l = 0; l < num_neurons; l++) {
+                    layer_bias_deltas[l] = layer_errors[l] * sigmoid_derivative(layer_outputs[l]);
+                }
+                for (int l = 0; l < num_weights_deltas; l++) {
+                    layer_weights_deltas_sum[l] = layer_weights_deltas[l] + layer_weights_deltas_sum_prev[l] * momentum;
+                }
+                for (int l = 0; l < num_bias_deltas; l++) {
+                    layer_bias_deltas_sum[l] = layer_bias_deltas[l] + layer_bias_deltas_sum_prev[l] * momentum;
+                }
+                for (int l = 0; l < num_weights_deltas; l++) {
+                    layer_weights_deltas_avg[l] = layer_weights_deltas_sum[l] / num_data;
+                }
+                for (int l = 0; l < num_bias_deltas; l++) {
+                    layer_bias_deltas_avg[l] = layer_bias_deltas_sum[l] / num_data;
+                }
+                for (int l = 0; l < num_weights_deltas; l++) {
+                    layer_weights[l] += layer_weights_deltas_avg[l] * learning_rate;
+                }
+                for (int l = 0; l < num_bias_deltas; l++) {
+                    network.layers[k].neurons[l].bias += layer_bias_deltas_avg[l] * learning_rate;
+                }
+                for (int l = 0; l < num_weights_deltas; l++) {
+                    layer_weights_deltas_sum_prev[l] = layer_weights_deltas_sum[l];
+                }
+                for (int l = 0; l < num_bias_deltas; l++) {
+                    layer_bias_deltas_sum_prev[l] = layer_bias_deltas_sum[l];
+                }
+                for (int l = 0; l < num_weights_deltas; l++) {
+                    layer_weights_deltas_avg_prev[l] = layer_weights_deltas_avg[l];
+                }
+                for (int l = 0; l < num_bias_deltas; l++) {
+                    layer_bias_deltas_avg_prev[l] = layer_bias_deltas_avg[l];
+                }
+                for (int l = 0; l < num_weights; l++) {
+                    network.layers[k].neurons[0].weights[l] = layer_weights[l];
+                }
+            }
+
+        }
+
+    }
+
+    // Test the network
+    outputs = network_output(&network, (double[]){0.0, 0.0});
+    printf("0.0, 0.0 -> %f\n", outputs[0]);
+    outputs = network_output(&network, (double[]){0.0, 1.0});
+    printf("0.0, 1.0 -> %f\n", outputs[0]);
+    outputs = network_output(&network, (double[]){1.0, 0.0});
+    printf("1.0, 0.0 -> %f\n", outputs[0]);
+    outputs = network_output(&network, (double[]){1.0, 1.0});
+    printf("1.0, 1.0 -> %f\n", outputs[0]);
+
+    return 0;
 }
